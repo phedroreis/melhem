30d6602574259c29e0b097e12cc982bbdc6bc461
Pedro Reis <ph3dror3is@gmail.com>
2023-12-04 11:50:15 -0300

    Primeiro Commit

---
+++ docs/js/main.js
@@ -0,0 +1,8 @@
+/*----------------------------------------------------------------------------
+            Listener responsavel por carregar paginas do site
+----------------------------------------------------------------------------*/
+function loadPage(page) {
+
+  window.open(page, "_self");//Carrega a pag. 
+
+}//loadPage()
